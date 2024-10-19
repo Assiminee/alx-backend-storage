d0b1d13a65ea9fb8d93ae8bd142f01654bf31340
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-20 00:29:34 +0100

    task0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- creates a table users with some criteria
+CREATE TABLE IF NOT EXISTS users {
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+}
