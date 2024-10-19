d73c332f591a50f4015f667584da5dad1e4a7216
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-20 00:31:55 +0100

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
 -- creates a table users with some criteria
-CREATE TABLE IF NOT EXISTS users {
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS users
+(
+    id    INT          NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
+    name  VARCHAR(255),
     PRIMARY KEY (id)
-}
+)
