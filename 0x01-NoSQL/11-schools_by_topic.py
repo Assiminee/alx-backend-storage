4bb9aae94079cdbc690f833612abfb955b7d5173
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-22 23:43:53 +0100

    Create 11-schools_by_topic.py

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Contains function schools_by_topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of schools having a specific topic
+    """
+    return mongo_collection.find({'topic': topic})
